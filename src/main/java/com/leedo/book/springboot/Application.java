f0c5a8b51a812bd8e4f68a0b574eadb1451994fa
kwaegeo <dlehgus113@naver.com>
2021-11-08 23:49:16 +0900

    Test 코드 수정

--- src/main/java/com/leedo/book/springboot/Application.java
+++ src/main/java/com/leedo/book/springboot/Application.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing //JPA Auditing 활성화
+//@EnableJpaAuditing삭제
+
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
--- src/test/java/com/leedo/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/leedo/book/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.leedo.book.springboot.web;
 
+import com.leedo.book.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -16,13 +20,14 @@ import static org.hamcrest.Matchers.is;
 
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class, excludeFilters = {@ComponentScan.Filter(type= FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)})
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
 
+    @WithMockUser(roles="USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -32,6 +37,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void hello_Dto가_리턴된다() throws Exception{
         String name = "hello";
